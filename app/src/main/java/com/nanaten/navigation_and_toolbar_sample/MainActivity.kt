6131bc3317cfb750310bac64e150b71fa1aa12c0
m.coder <kurokoshow00@gmail.com>
2020-02-13 14:10:10 +0900

    add comment

--- app/src/main/java/com/nanaten/navigation_and_toolbar_sample/MainActivity.kt
+++ app/src/main/java/com/nanaten/navigation_and_toolbar_sample/MainActivity.kt
@@ -19,17 +19,20 @@ class MainActivity : AppCompatActivity() {
         // Toolbarをセット
         setSupportActionBar(toolbar)
         val navController = findNavController(R.id.nav_host_fragment)
+        // UpIconを表示しないFragmentを設定する
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            // ThirdFragmentのみToolbarを消す
             toolbar.visibility = if(destination.id == R.id.thirdFragment) View.GONE else View.VISIBLE
+            // FirstFragmentのみロゴを表示させる
             toolbar.logo = if(destination.id == R.id.firstFragment) AppCompatResources.getDrawable(this, R.drawable.ic_android_24dp) else null
+            // タイトルを設定する
             if(destination.id == R.id.fifthFragment) {
                 supportActionBar?.title = arguments?.getString("title")
             }
         }
-
     }
 
     override fun onSupportNavigateUp(): Boolean =
